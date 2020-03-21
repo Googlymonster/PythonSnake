6df93b3c94edec695efe4ca273f7f537725232a3
kevin nguyen <kdnguyen9088@gmail.com>
2020-03-21 12:40:25 -0500

    updates

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Snake game written in Python
 
 ## Getting Started
 
-Before you clone the repository you first want to make sure you have [Python3](). Also make sure you have pygame installed as well.
+Before you clone the repository you first want to make sure you have [Python3](https://www.python.org/downloads/). Also make sure you have pygame installed as well.
 
 `pip3 install pygame`
 
@@ -19,3 +19,5 @@ To run the game enter the following code in the terminal.
 To move the snake around use the "wasd" keys.
 
 ### Have Fun
+
+![SnakeDemo](demo.gif)
--- snake.py
+++ snake.py
@@ -1,7 +1,6 @@
 import math
 import random
 import pygame
-# from pygame.locals import *
 import sys
 import tkinter as tk
 from tkinter import messagebox
@@ -50,31 +49,37 @@ class Snake(object):
 
     def move(self):
         for event in pygame.event.get():
+            print(event.type)
             if event.type == pygame.QUIT:
                 pygame.quit()
-            keys = pygame.key.get_pressed()
-            for key in keys:
-                if keys[pygame.K_a]:
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    print("Player moved left!")
                     self.dirnx = -1
                     self.dirny = 0
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                elif keys[pygame.K_d]:
+                elif event.key == pygame.K_d:
+                    print("Player moved right!")
                     self.dirnx = 1
                     self.dirny = 0
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                elif keys[pygame.K_w]:
+                elif event.key == pygame.K_w:
+                    print("Player moved up!")
                     self.dirnx = 0
                     self.dirny = -1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-                elif keys[pygame.K_s]:
+                elif event.key == pygame.K_s:
+                    print("Player moved down!")
                     self.dirnx = 0
                     self.dirny = 1
                     self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
         for i, c in enumerate(self.body):
             p = c.pos[:]
             if p in self.turns:
+                print(p)
                 turn = self.turns[p]
-                c.move(turn[0]. turn[1])
+                print(turn)
+                c.move(turn[0], turn[1])
                 if i == len(self.body)-1:
                     self.turns.pop(p)
             else:
@@ -103,7 +108,7 @@ class Snake(object):
         if dx == 1 and dy == 0:
             self.body.append(Cube((tail.pos[0]-1, tail.pos[1])))
         elif dx == -1 and dy == 0:
-            self.body.append(Cube((tail.post[0]+1, tail.pos[1])))
+            self.body.append(Cube((tail.pos[0]+1, tail.pos[1])))
         elif dx == 0 and dy == 1:
             self.body.append(Cube((tail.pos[0], tail.pos[1]-1)))
         elif dx == 0 and dy == -1:
@@ -177,11 +182,6 @@ def main():
     flag = True
     clock = pygame.time.Clock()
     while flag:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                flag = False
-                pygame.quit()
-                sys.exit()
         pygame.time.delay(50)
         clock.tick(10)
         s.move()
